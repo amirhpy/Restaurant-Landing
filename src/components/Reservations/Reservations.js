438e0971aa1d725fda42b64eaac93d1eed219032
amirhossein <amirhosseinhphpam@gmail.com>
2023-05-21 15:55:15 +0330

    fix bug v1.2

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -14,9 +14,9 @@ const Reservations = () => {
             <div className='w-4/5 text-white-0 flex flex-col items-center translate-x-[-50%] translate-y-[-50%] absolute left-[50%] top-[50%]'>
                 <h3 className='text-[30px]'>همین حالا رزرو کنید</h3>
                 <div className='w-full font-Vazir-digit flex flex-col my-[50px] sm:w-4/5 sm:flex-row lg:w-1/2'>
-                    <input name='date' value={data.date} onChange={changeHandler} className='flex-[1_1_100%] p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none' type='date' />
-                    <input name='time' value={data.time} onChange={changeHandler} className='flex-[1_1_100%] p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none my-[10px] sm:my-0 sm:mx-[10px]' type='time' />
-                    <select name='select' value={data.select} onChange={changeHandler} className='flex-[1_1_100%] p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none'>
+                    <input name='date' value={data.date} onChange={changeHandler} className='flex-[1_1_100%] w-full p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none' type='date' />
+                    <input name='time' value={data.time} onChange={changeHandler} className='flex-[1_1_100%] w-full p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none my-[10px] sm:my-0 sm:mx-[10px]' type='time' />
+                    <select name='select' value={data.select} onChange={changeHandler} className='flex-[1_1_100%] w-full p-[10px] bg-transparent-0 border-2 border-orange-0 outline-none'>
                         <option className='text-[#000]' value="person_2">2 نفر</option>
                         <option className='text-[#000]' value="person_3">3 نفر</option>
                         <option className='text-[#000]' value="person_4">4 نفر</option>
